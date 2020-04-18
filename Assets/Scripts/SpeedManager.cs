555d6b68b86ecda7de9413dec712b5cc20e9959f
krylov-na <krylov.nikita.a@yandex.ru>
2020-04-18 19:00:48 +0300

    Add speed manager

---
+++ Assets/Scripts/SpeedManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedManager : Singleton<SpeedManager>
+{
+    public float Speed { get; private set; } = 1f;
+
+    [SerializeField]
+    private float acceleration = 0.1f;
+
+    private void Update()
+    {
+        Speed += acceleration * Time.deltaTime;
+    }
+}
