68f17d98357a3a0d4693cd2aca1f860541733b6c
Alexander Voronkov <ax.voronkov@gmail.com>
2020-04-18 22:00:47 +0300

    Add moving terrain

---
+++ Assets/TerrainManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+    public Transform terrain1;
+    public Transform terrain2;
+    public SpeedManager speedManager;
+
+    void Update()
+    {
+        moveTerrain(terrain1);
+        moveTerrain(terrain2);
+    }
+
+    void moveTerrain(Transform t)
+    {
+        var rot = t.rotation;
+        var pos = t.position;
+        pos.x -= speedManager.Speed * Time.deltaTime;
+        if(pos.x < -21.5f)
+        {
+            pos.x = 21.5f;
+        }
+        t.SetPositionAndRotation(pos, rot);
+    }
+}
