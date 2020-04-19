e13dd7996fb813335f6fea7a8289e566ff85974c
krylov-na <krylov.nikita.a@yandex.ru>
2020-04-20 00:24:09 +0300

    Barrels and setup for highscore

--- Assets/Scripts/RandomObjectPool.cs
+++ Assets/Scripts/RandomObjectPool.cs
@@ -11,7 +11,7 @@ public class RandomObjectPool : BaseObjectPool
     protected override void Generate()
     {
         Vector3 lastActiveObjectPosition = activeObjects[activeObjects.Count - 1].transform.position;
-        float distance = Mathf.Abs(transform.position.x - lastActiveObjectPosition.x);
+        float distance = transform.position.x + generateDistance - lastActiveObjectPosition.x;
         if (distance > minDistance && Random.Range(0f, 1f) <= rarity)
         {
             Vector3 position = transform.position;
